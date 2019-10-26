649680da5f6a8a878d00c85c2c7c52c4948a7870
Hasantha malshan <hasanthamalshan07@gmail.com>
2019-10-26 14:36:42 +0530

    sparkle added for normal mode

--- Assets/Scripts/SpawnBoxes_Normal.cs
+++ Assets/Scripts/SpawnBoxes_Normal.cs
@@ -12,15 +12,30 @@ public class SpawnBoxes_Normal : MonoBehaviour
     bool boxtwo;
     GameObject obj1;
     GameObject obj2;
+
+    Vector3 sparkPos1;
+    Vector3 sparkPos2;
+
+    public GameObject sparkle;
+    Color sparkColor1;
+    Color sparkColor2;
        
     void Start(){
+        sparkle.SetActive(false);
         HealthBar.healthloss = 2.5f;
+
         Vector3 Boxpos1 = new Vector3(Random.Range(topLeft.position.x,bottomRight.position.x),Random.Range(bottomRight.position.y,topLeft.position.y),0); 
         obj1 = Instantiate(spawnee[2],Boxpos1,Quaternion.identity);
         obj1.tag = "one";
+        sparkPos1 = Boxpos1;
+        sparkColor1 = new Color(0.0549f, 0.9098f, 0.1098f, 1f);
+
         Vector3 Boxpos2 = new Vector3(Random.Range(topLeft.position.x,bottomRight.position.x),Random.Range(bottomRight.position.y,topLeft.position.y),0); 
         obj2 = Instantiate(spawnee[3],Boxpos2,Quaternion.identity);
         obj2.tag = "two";
+        sparkPos2 = Boxpos2;
+        sparkColor2 = new Color(0.0549f, 0.9098f, 0.1098f, 1f);
+
         isSpawnon = false;
         boxone = false;
         boxtwo = false;
@@ -34,11 +49,13 @@ public class SpawnBoxes_Normal : MonoBehaviour
          Collider2D hitCollider = Physics2D.OverlapPoint(Camera.main.ScreenToWorldPoint(pos));
             if (hitCollider != null) {
                 if( hitCollider.tag == "one"){
-                    GameObject.Destroy(obj1); 
+                    GameObject.Destroy(obj1);
+                    emitEffect(sparkColor1 , sparkPos1); 
                     boxone = true; 
                 }
                 if( hitCollider.tag == "two"){
-                    GameObject.Destroy(obj2); 
+                    GameObject.Destroy(obj2);
+                    emitEffect(sparkColor2 , sparkPos2);  
                     boxtwo = true;  
                 }
                 if(boxone && boxtwo){
@@ -50,15 +67,73 @@ public class SpawnBoxes_Normal : MonoBehaviour
             if(isSpawnon){
                 Vector3 Boxpos1 = new Vector3(Random.Range(topLeft.position.x,bottomRight.position.x),Random.Range(bottomRight.position.y,topLeft.position.y),0); 
                 obj1 = Instantiate(spawnee[num1],Boxpos1,Quaternion.identity);
+                sparkPos1 = Boxpos1;
+                setcolor1(num1);
                 obj1.tag = "one";
+
                 Vector3 Boxpos2 = new Vector3(Random.Range(topLeft.position.x,bottomRight.position.x),Random.Range(bottomRight.position.y,topLeft.position.y),0); 
                 obj2 = Instantiate(spawnee[num2],Boxpos2,Quaternion.identity);
+                sparkPos2 = Boxpos2;
+                setcolor2(num2);
                 obj2.tag = "two";
+
                 isSpawnon = false;
                 boxone = false;
                 boxtwo = false;
             }
         }
         
+    }
+
+    void emitEffect(Color col , Vector3 pos){
+        sparkle.GetComponent<ParticleSystem>().startColor = col;
+        sparkle.transform.position = pos;
+        sparkle.SetActive(true);
+        StartCoroutine(stopSparckles());
+    }
+
+    IEnumerator stopSparckles(){
+        yield return new WaitForSeconds(0.4f);
+        sparkle.SetActive(false);
+    }
+
+    void setcolor1(int num){
+        sparkColor1 = setcolor(num);
+    }
+    void setcolor2(int num){
+        sparkColor2 = setcolor(num);
+    }
+
+    Color setcolor(int num){
+        switch (num)
+         {
+            case 0:
+                return new Color(0.9f, 0.6f, 0.09f, 1f);
+                break;
+            case 1:
+                return new Color(0.0549f, 0.5529f, 0.9098f, 1f);
+                break;
+            case 2:
+                return new Color(0.0549f, 0.9098f, 0.1098f, 1f);
+                break;
+            case 3:
+                return new Color(0.7176f, 0.9686f, 0.0196f, 1f);
+                break;
+            case 4:
+                return new Color(0.9686f, 0.3215f, 0.0196f, 1f);
+                break;
+            case 5:
+                return new Color(0.9019f, 0.2627f, 0.2627f, 1f);
+                break;
+            case 6:
+                return new Color(0.9098f,0.0549f, 0.1843f, 1f);
+                break;
+            case 7:
+                return new Color(0.9294f,0.9411f, 0.1960f, 1f);
+                break;
+            default:
+                return new Color(0.3f, 0.4f, 0.6f, 0.3f);
+                break;
+         }
     }
 }
